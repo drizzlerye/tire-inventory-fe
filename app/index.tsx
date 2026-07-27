89ef5798c9d6d88e2e6b1342e1423a678ce41596
David Rye <drye84@gmail.com>
2026-07-27 11:49:00 -0500

    initial commit for project tire inventory project

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Welcome to the Tire Inventory App</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
